cc7aff59b257be19f45b62d3f5c42a2e314c9354
vietlongbkdp <vietlongbkdp@gmail.com>
2023-12-10 03:52:42 +0700

    update

--- src/main/java/com/cg/shoeprojectmain/controller/rest/ProductRestController.java
+++ src/main/java/com/cg/shoeprojectmain/controller/rest/ProductRestController.java
@@ -4,13 +4,14 @@ import com.cg.shoeprojectmain.model.*;
 import com.cg.shoeprojectmain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -25,8 +26,13 @@ public class ProductRestController {
             @RequestParam(name = "minPrice", required = false) Long minPrice,
             @RequestParam(name = "color", required = false) String color,
             @RequestParam(name = "search", required = false) String search,
+            @RequestParam(name = "page", required = false) int page,
+            @RequestParam(name = "size", required = false) int size,
+            @RequestParam(name = "sortField", required = false) String sortField,
+            @RequestParam(name = "direction", required = false) String direction,
             Pageable pageable
     ){
+            pageable = PageRequest.of(page, size, (direction.equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC), sortField);
             Page<Product> products = productRepository.filterProduct(category, company, new BigDecimal(maxPrice), new BigDecimal(minPrice), color, search.toLowerCase(), pageable);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
